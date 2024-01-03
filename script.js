5d0e2bfd7388f77bb055f047827589d30fd20bde
Vlastiss <120052173+Vlastiss@users.noreply.github.com>
2024-01-03 21:48:27 +0000

    update before sleep

---
+++ script.js
@@ -0,0 +1,38 @@
+const questions = [
+  {
+    question: "What year was the American revolution?",
+    answers: [
+      { text: "1774", correct: false },
+      { text: "1775", correct: false },
+      { text: "1776", correct: true },
+      { text: "1778", correct: false }
+    ]
+  },
+  {
+    question: "What is the hottest place on the world?",
+    answers: [
+      { text: "Death Valley", correct: true },
+      { text: "Antarctica", correct: false },
+      { text: "Ur-anus", correct: true },
+      { text: "Sahara", correct: false }
+    ]
+  },
+  {
+    question: "Which animal was on planet before trees?",
+    answers: [
+      { text: "Elephant", correct: false },
+      { text: "Shark", correct: true },
+      { text: "Monkey", correct: false },
+      { text: "Dog", correct: false }
+    ]
+  },
+  {
+    question: "Which state has Alps mountains on the south border?",
+    answers: [
+      { text: "France", correct: false },
+      { text: "Italy", correct: false },
+      { text: "Japan", correct: false },
+      { text: "Germany", correct: true }
+    ]
+  }
+]
